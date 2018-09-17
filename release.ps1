1038e3e6b4781eda389c0712d93c76d385929ccc
Daniel Oliver <danieloliversoftware@gmail.com>
2018-09-16 20:56:31 -0400

    Use 7zip to make tar

--- release.ps1
+++ release.ps1
@@ -6,16 +6,37 @@ $githubToken=$env:githubToken
 
 Remove-Item src/server/bin -Force -Recurse -ErrorAction SilentlyContinue;
 Remove-Item zip -Force -Recurse -ErrorAction SilentlyContinue;
+mkdir zip;
+
 dotnet publish src/server -c Release -r win-x64 --self-contained -o "./bin/win_x64";
 dotnet publish src/server -c Release -r linux-x64 --self-contained -o "./bin/linux_x64";
 
-mkdir zip;
+
+function Create-Tar($tarFile, $dest) {
+    Compress-7Zip $dest $tarFile TAR
+}
 $winx64zip = "mssql_exporter_win_x64.zip";
 $winx64zipPath = "zip/$winx64zip";
-$linuxx64zip = "mssql_exporter_linux_x64.zip";
+# $linuxx64zip = "mssql_exporter_linux_x64.zip";
+$linuxx64zip = "mssql_exporter_linux_x64.tar";
 $linuxx64zipPath = "zip/$linuxx64zip";
 Compress-Archive -Path src/server/bin/win_x64/* -DestinationPath $winx64zipPath -Force;
-Compress-Archive -Path src/server/bin/linux_x64/* -DestinationPath $linuxx64zipPath -Force;
+# Compress-Archive -Path src/server/bin/linux_x64/* -DestinationPath $linuxx64zipPath -Force;
+$originalPath = Get-Location
+try {
+    if (-not (Get-Command Compress-7Zip -ErrorAction Ignore)) {
+        Save-Module -Name 7Zip4Powershell -Path . -RequiredVersion 1.9.0 -Force;
+        $pathToModule = ".\7Zip4Powershell\1.9.0\7Zip4PowerShell.psd1";
+        $pathToModule = Resolve-Path $(Join-Path $(Get-Location) $pathToModule);
+        Import-Module -Name $pathToModule;
+    }
+    Compress-7Zip -ArchiveFileName $linuxx64zipPath -Path "./src/server/bin/linux_x64/" -Format TAR
+    # Move-Item $linuxx64zip "zip/"
+
+}
+finally {
+    Set-Location $originalPath;
+}
 
 if($hasTag -eq "true") {
     Write-Host "Finding release.";
@@ -33,6 +54,7 @@ if($hasTag -eq "true") {
     Write-Host "Uploading linux_x64 release Asset";
     Invoke-WebRequest -Uri $linuxx64AssetUrl -Method Post -InFile $linuxx64zipPath -ContentType "application/octet-stream" -Headers @{"Authorization"="token $githubToken"}
     Write-Host "Uploaded release assets";
+
 }
 
 
